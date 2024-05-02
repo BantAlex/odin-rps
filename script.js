31ef3aea9b5fea261d35a6442a4c4f5773e416f2
Alex Bandis <bantalex3@gmail.com>
2024-05-02 19:41:30 +0300

    Create some of play-round function. Add the case of a tie.

--- script.js
+++ script.js
@@ -3,7 +3,11 @@
 //-Create a function that generates a random answer between the three. -Check-
 //Add win and lose states/messages.
 
-function getComputerChoice(){
+let humanScore = 0;
+let computerScore = 0;
+
+
+function getComputerChoice() {
     let computerChoice = ''
     let randomNum = Math.floor((Math.random() * 3) + 1 )
 
@@ -15,18 +19,31 @@ function getComputerChoice(){
         computerChoice = "Scissors"
     }
 
-    console.log(computerChoice)
+    console.log(computerChoice);
+    return computerChoice;
 }
 
+
 function getHumanChoice() {
-    getComputerChoice();
-    let userChoice = prompt("Please make your move: Rock, Paper or Scissors?")
+    let humanChoice = prompt("Please make your move: Rock, Paper or Scissors?")
     
-    if (userChoice.toLowerCase() != "rock" || userChoice.toLowerCase() != "paper" || userChoice.toLowerCase() != "Scissors"){
+    if (humanChoice.toLowerCase() != "rock" || humanChoice.toLowerCase() != "paper" || humanChoice.toLowerCase() != "Scissors"){
         console.log("Wrong input. Please try again.")
         getHumanChoice();
     }
+
+    return humanChoice;
 }
 
-getHumanChoice();
 
+function playRound() {
+
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+
+    if (computerChoice === humanChoice) {
+        console.log("It's a Tie!")
+    }
+}
+
+playRound();
