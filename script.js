3c53c89369d5c591061529570ad2af6a6998a549
Alex Bandis <bantalex3@gmail.com>
2024-05-02 21:26:02 +0300

    Debug phase: Choices dont seem to work properly. Set-up the files so that i can re-write the play-round function from skratch

--- script.js
+++ script.js
@@ -1,98 +1,116 @@
-//-Take input of rock/paper/scissors from the user. 
-//-Make sure that these are the only values allowed. 
-//-Create a function that generates a random answer between the three. -Check-
-//Add win and lose states/messages.
 
-let humanScore = 0;
-let computerScore = 0;
-//Computer Choice and Human Choice functions
-function getComputerChoice() {
-    let computerChoice = ''
-    let randomNum = Math.floor((Math.random() * 3) + 1 )
+        //-Take input of rock/paper/scissors from the user. -Check- *Kinda
+        //-Make sure that these are the only values allowed. -Check-
+        //-Create a function that generates a random answer between the three. -Check-
+        //-Add win and lose states/messages. -Check-
 
-    if (randomNum === 1) {
-        computerChoice = 'Rock';
-    } else if (randomNum === 2) {
-        computerChoice = "Paper";
-    } else {
-        computerChoice = "Scissors"
-    }
+        let humanScore = 0;
+        let computerScore = 0;
 
-    return computerChoice.toLowerCase();
-}
-
-
-function getHumanChoice() {
-    let humanChoice = prompt("Please make your move: Rock, Paper or Scissors?")
-    
-    if (humanChoice.toLowerCase() != "rock" || humanChoice.toLowerCase() != "paper" || humanChoice.toLowerCase() != "scissors"){
-        console.log("Wrong input. Please try again.")
-        getHumanChoice();
-    }
-
-    return humanChoice.toLowerCase();
-}
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-
-//Round function and start
-
-function playRound(computerChoice,humanChoice) {
-
-    if (computerChoice === humanChoice) {
-
-        console.log("It's a Tie!")
-        playRound();
-
-    } else if (computerChoice === "rock" && computerChoice === 'paper'){
-
-        humanScore++
-        console.log("You win! You gain one point!")
-        console.log("You currently have " + humanScore + " points")
-        console.log("I currently have " + computerScore + " points")
-        playRound();
-
-    } else if (computerChoice === "rock" && computerChoice === 'scissors'){
-
-        computerScore++
-        console.log("I win! I gain a point!")
-        console.log("You currently have " + humanScore + " points")
-        console.log("I currently have " + computerScore + " points")
-        playRound();
-
-    } else if (computerChoice === "paper" && computerChoice === 'scissors'){
-
-        humanScore++
-        console.log("You win! You gain one point!")
-        console.log("You currently have " + humanScore + " points")
-        console.log("I currently have " + computerScore + " points")
-        playRound();
+        //Computer Choice and Human Choice functions
         
-    } else if (computerChoice === "paper" && computerChoice === 'rock'){
+        function getComputerChoice() {
 
-        computerScore++
-        console.log("I win! I gain a point!")
-        console.log("You currently have " + humanScore + " points")
-        console.log("I currently have " + computerScore + " points")
+            let computerChoice = ''
+            let randomNum = Math.floor((Math.random() * 3) + 1 )
+
+            if (randomNum === 1) {
+                computerChoice = 'Rock';
+            } else if (randomNum === 2) {
+                computerChoice = "Paper";
+            } else {
+                computerChoice = "Scissors"
+            }
+
+            return computerChoice.toLowerCase();
+        }
+
+
+        function getHumanChoice() {
+
+            let humanChoice = prompt("Please make your move: Rock, Paper or Scissors?")
+            
+            if (humanChoice.toLowerCase() !== "rock" && humanChoice.toLowerCase() !== "paper" && humanChoice.toLowerCase() !== "scissors"){
+                console.log("Wrong input. Please try again.")
+                getHumanChoice();
+            }
+
+            return humanChoice.toLowerCase();
+        }
+            
+            
+
+        //Round function and start
+
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+
+        function playRound(computerChoice,humanChoice) {
+            
+            getComputerChoice();
+            getHumanChoice();
+     
+            if (computerChoice === humanChoice) {
+
+                console.log("It's a Tie!");
+                console.log("You currently have " + humanScore + " points");
+                console.log("I currently have " + computerScore + " points");
+                playRound();
+
+            } else if (computerChoice === "rock" && humanChoice === 'paper'){
+
+                humanScore++;
+                console.log("You win! You gain one point!");
+                console.log("You currently have " + humanScore + " points");
+                console.log("I currently have " + computerScore + " points");
+                playRound();
+
+            } else if (computerChoice === "rock" && humanChoice === 'scissors'){
+
+                computerScore++;
+                console.log("I win! I gain a point!");
+                console.log("You currently have " + humanScore + " points");
+                console.log("I currently have " + computerScore + " points");
+                playRound();
+
+            } else if (computerChoice === "paper" && humanChoice === 'scissors'){
+
+                humanScore++;
+                console.log("You win! You gain one point!");
+                console.log("You currently have " + humanScore + " points");
+                console.log("I currently have " + computerScore + " points");
+                playRound();
+                
+            } else if (computerChoice === "paper" && humanChoice === 'rock'){
+
+                computerScore++;
+                console.log("I win! I gain a point!");
+                console.log("You currently have " + humanScore + " points");
+                console.log("I currently have " + computerScore + " points");
+                playRound();
+
+            } else if (computerChoice === "scissors" && humanChoice === 'rock') {
+
+                humanScore++;
+                console.log("You win! You gain one point!");
+                console.log("You currently have " + humanScore + " points");
+                console.log("I currently have " + computerScore + " points");
+                playRound();
+                
+            } else if (computerChoice === "scissors" && humanChoice === 'paper') {
+
+                computerScore++;
+                console.log("I win! I gain a point!");
+                console.log("You currently have " + humanScore + " points");
+                console.log("I currently have " + computerScore + " points");
+                playRound();
+
+            } else {
+                console.log("Placeholder");
+            }
+
+            playRound();
+
+        }
+
         playRound();
-
-    } else if (computerChoice === "scissors" && computerChoice === 'paper') {
-
-        humanScore++
-        console.log("You win! You gain one point!")
-        console.log("You currently have " + humanScore + " points")
-        console.log("I currently have " + computerScore + " points")
-        playRound();
-        
-    } else if (computerChoice === "scissors" && computerChoice === 'rock') {
-
-        computerScore++
-        console.log("I win! I gain a point!")
-        console.log("You currently have " + humanScore + " points")
-        console.log("I currently have " + computerScore + " points")
-        playRound();
-
-    }
-}
-
-playRound();
