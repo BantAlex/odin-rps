778069f67598b3f6295c684e8d214fdd60ef92ea
Alex Bandis <bantalex3@gmail.com>
2024-05-02 19:20:17 +0300

    Wrong commnets

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-//-Take input of rock/paper/scissors from the user.
-//-Make sure that these are the only values allowed.
-//-Create a function that generates between the three.
+//-Take input of rock/paper/scissors from the user. 
+//-Make sure that these are the only values allowed. 
+//-Create a function that generates a random answer between the three. -Check-
 //Add win and lose states/messages.
 
 function getComputerChoice(){
-    let choice = ''
+    let computerChoice = ''
     let randomNum = Math.floor((Math.random() * 3) + 1 )
 
     if (randomNum === 1) {
-        choice = 'Rock';
+        computerChoice = 'Rock';
     } else if (randomNum === 2) {
-        choice = "Paper";
+        computerChoice = "Paper";
     } else {
-        choice = "Scissors"
+        computerChoice = "Scissors"
     }
 
-    console.log(choice);
+    console.log(computerChoice)
 }
+
+function getHumanChoice() {
+    getComputerChoice();
+    let userChoice = prompt("Please make your move:")
+    
+    if (userChoice.toLowerCase() != "rock" || userChoice.toLowerCase() != "paper" || userChoice.toLowerCase() != "Scissors"){
+        console.log("Wrong input. Please try again.")
+        getHumanChoice();
+    }
+}
+
+getHumanChoice();
 
