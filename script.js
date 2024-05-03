a415ffb56cd8b4edcf7a1f33354e433b99003f53
Alex Bandis <bantalex3@gmail.com>
2024-05-03 19:32:57 +0300

    Add a five round game with the ability to replay

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
         //Computer Choice and Human Choice functions
 
 
-        function getComputerChoice() {
+        function getComputerChoice(){
 
             let computerChoice = ''
             let randomNum = Math.floor((Math.random() * 3) + 1 )
@@ -27,7 +27,7 @@
         }
 
 
-        function getHumanChoice() {
+        function getHumanChoice(){
 
             let humanChoice = prompt("Please make your move: Rock, Paper or Scissors?")
             
@@ -39,67 +39,68 @@
             return humanChoice.toLowerCase();
         }
             
-            
+            let compSelection = getComputerChoice();
+            let humSelection = getHumanChoice();
 
         //Round function and start
 
         function playRound() {
             
 
-            if (getComputerChoice() === getHumanChoice()) {
+            if (compSelection === humSelection) {
 
+                console.log("I picked " + compSelection + "!")
                 console.log("It's a Tie!");
                 console.log("You currently have " + humanScore + " points");
                 console.log("I currently have " + computerScore + " points");
-                playRound();
 
-            } else if (getComputerChoice() === "rock" && getHumanChoice() === 'paper'){
+            } else if (compSelection === "rock" && humSelection=== 'paper'){
 
                 humanScore++;
+                console.log("I picked " + compSelection + "!")
                 console.log("You win! You gain one point!");
                 console.log("You currently have " + humanScore + " points");
                 console.log("I currently have " + computerScore + " points");
-                playRound();
 
-            } else if (getComputerChoice() === "rock" && getHumanChoice() === 'scissors'){
+            } else if (compSelection === "rock" && humSelection=== 'scissors'){
 
                 computerScore++;
+                console.log("I picked " + compSelection + "!")
                 console.log("I win! I gain a point!");
                 console.log("You currently have " + humanScore + " points");
                 console.log("I currently have " + computerScore + " points");
-                playRound();
 
-            } else if (getComputerChoice() === "paper" && getHumanChoice() === 'scissors'){
+            } else if (compSelection === "paper" && humSelection=== 'scissors'){
 
                 humanScore++;
+                console.log("I picked " + compSelection + "!")
                 console.log("You win! You gain one point!");
                 console.log("You currently have " + humanScore + " points");
                 console.log("I currently have " + computerScore + " points");
-                playRound();
                 
-            } else if (getComputerChoice() === "paper" && getHumanChoice() === 'rock'){
+            } else if (compSelection === "paper" && humSelection=== 'rock'){
 
                 computerScore++;
+                console.log("I picked " + compSelection + "!")
                 console.log("I win! I gain a point!");
                 console.log("You currently have " + humanScore + " points");
                 console.log("I currently have " + computerScore + " points");
-                playRound();
 
-            } else if (getComputerChoice() === "scissors" && getHumanChoice() === 'rock') {
+            } else if (compSelection === "scissors" && humSelection=== 'rock') {
 
                 humanScore++;
+                console.log("I picked " + compSelection + "!")
                 console.log("You win! You gain one point!");
                 console.log("You currently have " + humanScore + " points");
                 console.log("I currently have " + computerScore + " points");
-                playRound();
                 
-            } else if (getComputerChoice() === "scissors" && getHumanChoice() === 'paper') {
+            } else if (compSelection === "scissors" && humSelection=== 'paper') {
 
                 computerScore++;
+                console.log("I picked " + compSelection + "!")
                 console.log("I win! I gain a point!");
                 console.log("You currently have " + humanScore + " points");
                 console.log("I currently have " + computerScore + " points");
-                playRound();
 
             } else {
                 console.log("Placeholder");
@@ -108,7 +109,58 @@
         }
         
                 //Computer Choice does not change no matter how many times the funtion is called.
-        playRound();
+        function playGame() {
 
+            //Round 1
+            playRound();
+            getComputerChoice();
+            getHumanChoice();
+
+            //Round 2
+            playRound();
+            getComputerChoice();
+            getHumanChoice();
+
+            //Round 3
+            playRound();
+            getComputerChoice();
+            getHumanChoice();
+
+            //Round 4
+            playRound();
+            getComputerChoice();
+            getHumanChoice();
+
+            //Round 5
+            playRound();
+            getComputerChoice();
+            getHumanChoice();
+           
+            //Replay Function
+            function replayFunc(){
+
+                let replay = prompt("Play again? (Yes/No)");
+
+                if (replay.toLowerCase() === "yes"){
+                    playGame();
+                    return replay;
+                } else if (replay.toLowerCase() === "no"){
+                    console.log("Okay, goodbye!")
+                    return;
+                } else{
+                    console.log("Not a valid option. Please input either YES or NO.")
+                    replayFunc();
+                }
+            }
+            if (humanScore > compSelection) {
+                console.log("Congrats! You win nothing at all!")
+                replayFunc();
+            } else {
+                console.log("You lose...sorry")
+                replayFunc();
+            }
+        }
+
+        playGame();
     
         //It seems to generate it here and then it stops doing so when called.
